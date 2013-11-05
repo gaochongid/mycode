3582893e9d7179e2a41742b16c360d9dec4c842e
gaochong <gaochong@adsage.com>
2013-11-05 17:41:33 +0800

    rename js-1.6.js to js-array.js

--- ecma/js-array.js
+++ ecma/js-array.js
@@ -1,5 +1,37 @@
-var arr = [1, 2, 3, 1, 4, 2];
+var arr = [1, 2, 3, '100', 1, 4, 2];
 
+// sort
+var sarr = arr.sort(function ( a, b ) {
+	return a - b;
+	// return b - a;
+});
+console.log(sarr.toString());// 1,1,2,2,3,4,100
+
+// splice
+var rm = arr.splice(2, 0, 8);
+console.log(rm); // []
+
+console.log(arr);
+var rm = arr.splice(3, 2);
+console.log(rm);
+
+
+// concat
+var narr = arr.concat();
+console.log(narr == arr); // false
+console.log(narr.join() == arr.join()); // true
+
+var arr1 = ['a', 'b', 'c'];
+var narr = arr.concat(arr1);
+console.log(narr.join()); // 1,2,3,100,1,4,2,a,b,c
+
+var arr2 = ['a1', 'a2', 'a3'];
+var narr = arr.concat(arr1, arr2);
+console.log(narr.join()); // 1,2,3,100,1,4,2,a,b,c,a1,a2,a3
+
+
+
+// ==============js1.6=============
 arr.indexOf(1);
 arr.lastIndexOf(1);
 
@@ -35,4 +67,6 @@ arr.forEach(function (val, index, arr) {
 // 在数组中的每个项上运行一个函数，并将全部结果作为数组返回。
 arr.map(function (val, index, arr) {
 	return val+1;
-});
+});
+// ==============js1.6=============
+
