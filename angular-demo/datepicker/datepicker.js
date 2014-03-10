71383786a0c8e4f0a07f8c0ac9d5d1164181a852
gaochong <gaochong@fyec.cn>
2014-03-10 14:39:25 +0800

    angular datepicker

---
+++ angular-demo/datepicker/datepicker.js
@@ -0,0 +1,48 @@
+angular.module('myApp.directives', []).directive('datepicker', function () {
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		// scope: false 现有的作用域，默认方式
+		// scope: true 新的作用域
+		// 独立作用域
+		// 默认情况下不可以访问父scope中的任何东西
+		// 可以通过以下三种方式（绑定策略）传递指令中
+		// &传递方法, @ | =传递属性
+		// 当创建可复用的组件时使用独立作用域
+		scope: {
+			select: '&',
+			date1: '@dateS1',
+			date2: '=dateS2'
+		},
+		link: function ( scope, element, attrs, ngModel ) {
+			if ( !ngModel ) { return false; }
+
+			var optObj = {};
+
+			optObj.dateFormat = 'mm/dd/yy';
+
+			var updateModel = function ( dateTxt ) {
+				scope.$apply(function () {
+					ngModel.$setViewValue(dateTxt);
+				});
+			};
+
+			optObj.onSelect = function ( dateTxt, picker ) {
+				updateModel(dateTxt);
+				if ( scope.select ) {
+					scope.$apply(function () {
+						scope.select({date: dateTxt});
+					});
+				}
+				console.log('date1: ', scope.date1)
+				console.log('date2: ', scope.date2)
+			};
+
+			ngModel.$render = function () {
+				element.datepicker('setDate', ngModel.$viewValue);
+			};
+
+			element.datepicker(optObj);
+		}
+	};
+});
